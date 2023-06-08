a4eff03b5f6cdc39a8f11dc3dd3ecdaf08b6b8e2
bleep123 <125577329+bleep123@users.noreply.github.com>
2023-06-08 11:38:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+hamburger = document.querySelector('.hamburger');
+menu = document.querySelector('.menu');
+navig = document.querySelector('.navig');
+
+hamburger.addEventListener('click', ()=>{
+menu.classList.toggle('v-class');
+ 
+});
+
